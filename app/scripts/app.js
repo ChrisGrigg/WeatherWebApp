6271b30352321ccb68229d8dbd344bde59dac095
Christopher Grigg <christophergrigg@Christophers-MacBook-Pro.local>
2015-05-09 14:37:11 +1000

    Simplified app and added local bootstrap
    The process is now simplified relating to saving data to Arrays.
    LocalStorage is not being used as it’s better to have something simple
    to work from.
    
    Added Bootstrap and JQuery libs manually.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,86 +1,88 @@
-/**
-* Created by Christopher Grigg 04/01/2015
-*/
-
-//var CHRISG = CHRISG || {};
-
 (function ()
 {
   'use strict';
-  
-  var app = angular.module('site', []);
-  var citiesArray = []; // holds information for list, to loop through
-//  var weatherData = [];
-  
-  app.service('CitiesService', function() {
-    //to create unique contact id
+  var module = angular.module('app', []);
+ 
+module.service('CitiesService', function () {
+    //to create unique city id
     var uid = 1;
-    var cities = [];
-    var savedCities;
-    
-    savedCities = localStorage.getItem('cities');
-	  cities = (localStorage.getItem('cities')!==null) ? JSON.parse(savedCities) : ['Brisbane'];
-    localStorage.setItem('cities', JSON.stringify(cities));
-
-    this.save = function(field) {
-      cities.push(field.city);
-      field.city = ''; //clear the input after adding
-      localStorage.setItem('cities', JSON.stringify(cities));
-    };
-    
-    //simply returns the contacts list
+     
+    //cities array to hold list of all cities
+    var cities = [{
+        id: 0,
+        'name': 'Brisbane'
+    }];
+     
+    //save method create a new city if not already exists
+    //else update the existing object
+    this.save = function (city) {
+        if (city.id == null) {
+            //if this is new city, add it in cities array
+            city.id = uid++;
+            cities.push(city);
+        } else {
+            //for existing city, find this city using id
+            //and update it.
+            for (var i in cities) {
+                if (cities[i].id == city.id) {
+                    cities[i] = city;
+                }
+            }
+        }
+ 
+    }
+ 
+    //simply search cities list for given id
+    //and returns the city object if found
+    this.get = function (id) {
+        for (var i in cities) {
+            if (cities[i].id == id) {
+                return cities[i];
+            }
+        }
+ 
+    }
+     
+    //iterate through cities list and delete 
+    //city if found
+    this.delete = function (id) {
+        for (var i in cities) {
+            if (cities[i].id == id) {
+                cities.splice(i, 1);
+            }
+        }
+    }
+ 
+    //simply returns the cities list
     this.list = function () {
-      return cities;
+        return cities;
     }
-  });
-  
-  app.service('WeatherService', function() {
-    var weatherList = [];
-    
-    this.addCity = function(city) {
-      weatherList.push(city);
-    }
-    
-    this.list = function() {
-      return weatherList;
-    }
-  });
-  
-  app.controller('CitiesController', ['$scope', '$http', 'CitiesService', 'WeatherService', function ($scope, $http, CitiesService, WeatherService) {
-
+});
+ 
+module.controller('CityController', ['$scope', '$http', 'CitiesService', function ($scope, $http, CitiesService) {
+ 
     $scope.cities = CitiesService.list();
-    
-    $scope.getWeatherData = function () {
-           
-      if($scope.field) {
-        CitiesService.save($scope.field);
-        $scope.field = {};
-      }
+ 
+    $scope.saveCity = function () {
       
-      for (var i=0; i<CitiesService.list().length; i++) {
-        $http.get('http://api.openweathermap.org/data/2.5/weather?q='+CitiesService.list()[i])
+      $http.get('http://api.openweathermap.org/data/2.5/weather?q='+$scope.newCity.name)
             .success(function(data)
             {
-                WeatherService.addCity(data);
+                $scope.newCity.weather = data;
+                CitiesService.save($scope.newCity);
+                $scope.newCity = {};
             })
             .error(function()
             {
                 alert('error loading weatherAppCtrl json');
             });
-      }
-    };
-    $scope.getWeatherData();
-    
-//    $scope.delete = function (id) {
-//        CitiesService.delete(id);
-//        if ($scope.field.id == id) $scope.field = {};
-//    }
-//
-//    $scope.edit = function (id) {
-//        $scope.field = angular.copy(CitiesService.get(id));
-//    }
+    }
+ 
+    $scope.delete = function (id) {
+ 
+        CitiesService.delete(id);
+        if ($scope.newCity.id == id) $scope.newCity = {};
+    }
+}])
 
-    $scope.weatherList = WeatherService.list();
-  }]);
-  
 })();
